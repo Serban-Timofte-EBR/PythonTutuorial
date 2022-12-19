52ae58907184b586b53789aea3a5273473be938e
Serban-Timofte-EBR <108286629+Serban-Timofte-EBR@users.noreply.github.com>
2022-12-19 14:20:18 +0200

    Update map_filter_zip_reduce.py

--- map_filter_zip_reduce.py
+++ map_filter_zip_reduce.py
@@ -12,6 +12,8 @@ def accumulator(acc, item):
 #1 Capitalize all of the pet names and print the list
 my_pets = ['sisi', 'bibi', 'titi', 'carla']
 print(list(map(capitalize, my_pets)))
+#1.1 Lamba version
+print(list(map(lambda item:item.upper(), my_pets)))
 
 #2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
 my_strings = ['a', 'b', 'c', 'd', 'e']
@@ -23,8 +25,11 @@ print(sorted(zipped_list, reverse = True))
 #3 Filter the scores that pass over 50%
 scores = [73, 20, 65, 19, 76, 100, 88]
 print(list(filter(higherfct, scores)))
+#3.1 Lamba version
+print(list(filter(lambda item: item>50, scores)))
 
 #4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
-
 numbers = my_numbers + scores
 print(reduce(accumulator, numbers))
+#4.1 Labmda version
+print(reduce(lambda acc,item: acc+item, numbers))
