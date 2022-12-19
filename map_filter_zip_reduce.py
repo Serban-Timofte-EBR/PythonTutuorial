81c35f6dfb93105d9a57639da12f835fee0b314c
Serban-Timofte-EBR <108286629+Serban-Timofte-EBR@users.noreply.github.com>
2022-12-19 14:15:16 +0200

    Create map_filter_zip_reduce.py

---
+++ map_filter_zip_reduce.py
@@ -0,0 +1,30 @@
+from functools import reduce
+
+def capitalize(item):
+  return item.upper()
+
+def higherfct(item):
+  return item > 50
+
+def accumulator(acc, item):
+  return acc+item
+
+#1 Capitalize all of the pet names and print the list
+my_pets = ['sisi', 'bibi', 'titi', 'carla']
+print(list(map(capitalize, my_pets)))
+
+#2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
+my_strings = ['a', 'b', 'c', 'd', 'e']
+my_numbers = [5,4,3,2,1]
+
+zipped_list = list(zip(my_strings, my_numbers))
+print(sorted(zipped_list, reverse = True))
+
+#3 Filter the scores that pass over 50%
+scores = [73, 20, 65, 19, 76, 100, 88]
+print(list(filter(higherfct, scores)))
+
+#4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+
+numbers = my_numbers + scores
+print(reduce(accumulator, numbers))
